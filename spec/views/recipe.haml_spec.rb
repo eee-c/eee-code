2443482f5f62041aa7e20185aedeac051074df51
eee-c <eee.c@eeecooks.com>
2009-03-28 21:56:09 -0400

    Address a boundary condition in the view spec.

--- spec/views/recipe.haml_spec.rb
+++ spec/views/recipe.haml_spec.rb
@@ -3,18 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper' )
 describe "recipe.haml" do
   before(:each) do
     @title  = "Recipe Title"
-
-    @egg_preparation = {
-      'quantity' => 1,
-      'ingredient' => { 'name' => 'egg' }
-    }
-
-    @recipe = {
-      'title' => @title,
-      'preparations' => [ @egg_preparation ]
-    }
-
-    assigns[:recipe] = @recipe
+    assigns[:recipe] = @recipe = { 'title' => @title }
   end
 
   it "should display the recipe's title" do
@@ -23,10 +12,25 @@ describe "recipe.haml" do
   end
 
   it "should render ingredient names" do
+    @recipe['preparations'] =
+      [  'quantity' => 1, 'ingredient' => { 'name' => 'egg' } ]
+
     render("views/recipe.haml")
+
     response.should have_selector(".preparations") do |preparations|
       preparations.
         should have_selector(".ingredient > .name", :content => 'egg')
+    end
+  end
+
+  context "a recipe with no ingredient preparations" do
+    before(:each) do
+      @recipe[:preparations] = nil
+    end
+
+    it "should not render an ingredient preparations" do
+      render("views/recipe.haml")
+      response.should_not have_selector(".preparations")
     end
   end
 end
