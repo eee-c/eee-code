b168fbc699969745a4499ce167b1ec2b28636f1b
eee-c <eee.c@eeecooks.com>
2009-03-30 20:33:41 -0400

    Implement given step for the next recipe details scenario.

--- features/step_definitions/recipe_details.rb
+++ features/step_definitions/recipe_details.rb
@@ -35,6 +35,23 @@ Given /^a recipe for Buttermilk Chocolate Chip Pancakes$/ do
     :content_type => 'application/json'
 end
 
+Given /^a recipe for Crockpot Lentil Andouille Soup$/ do
+  @date = Date.new(2009, 3, 24)
+  @title = "Crockpot Lentil Andouille Soup"
+  @permalink = @date.to_s + "-" + @title.downcase.gsub(/\W/, '-')
+
+  recipe = {
+    :title         => @title,
+    :date          => @date,
+    :inactive_time => 300,
+    :prep_time     => 15
+  }
+
+  RestClient.put "#{@@db}/#{@permalink}",
+    recipe.to_json,
+    :content_type => 'application/json'
+end
+
 When /^I view the recipe$/ do
   visit("/recipes/#{@permalink}")
 end
@@ -45,4 +62,8 @@ Then /^I should see an ingredient of "(.+)"$/ do |ingredient|
     gsub(/\(/, "\\(").
     gsub(/\)/, "\\)")
   response.should contain(Regexp.new(matcher))
+end
+
+Then /^I should see 15 minutes of prep time$/ do
+  pending
 end
