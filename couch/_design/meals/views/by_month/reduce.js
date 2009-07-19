2d8df93fedc98fce984f160048eb31e78b5d947c
eee-c <eee.c@eeecooks.com>
2009-07-18 21:19:16 -0400

    Functioning reduce (and rereduce) function for meals-by-month

--- couch/_design/meals/views/by_month/reduce.js
+++ couch/_design/meals/views/by_month/reduce.js
@@ -1,1 +1,14 @@
-function(keys, values, rereduce) { return values; }
+function(keys, values, rereduce) {
+  if (rereduce) {
+    var a = [];
+    for (var i=0; i<values.length; i++) {
+      for (var j=0; j<values[i].length; j++) {
+        a.push(values[i][j]);
+      }
+    }
+    return a;
+  }
+  else {
+    return values;
+  }
+}
