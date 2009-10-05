4a0dc217bfa22f8485b4bd7576a35a84c08359be
eee-c <eee.c@eeecooks.com>
2009-10-04 21:38:57 -0400

    Do not include ingredients that are previous recipes in the ingredients index.

--- couch/_design/recipes/views/by_ingredients/map.js
+++ couch/_design/recipes/views/by_ingredients/map.js
@@ -3,7 +3,8 @@ function (doc) {
     for (var i in doc['preparations']) {
       var ingredient = doc['preparations'][i]['ingredient']['name'];
       var value      = [doc['_id'], doc['title']];
-      emit(ingredient, {"id":doc['_id'],"title":doc['title']});
+      if (ingredient.indexOf('[recipe:') == -1)
+        emit(ingredient, {"id":doc['_id'],"title":doc['title']});
     }
   }
 }
