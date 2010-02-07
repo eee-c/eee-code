c4dbe41303431dd3dee9b9d3f726188a8f411c72
Chris Strom <github@eeecooks.com>
2010-02-06 21:30:08 -0500

    New couchdb-lucene design document and some minor tweaks to the code to support the new couchdb-lucene API

---
+++ couch/_design/recipes/fulltext/all/analyzer.js
@@ -0,0 +1,1 @@
+perfield:{default:"porter"}
---
+++ couch/_design/recipes/fulltext/all/index.js
@@ -0,0 +1,46 @@
+function(rec) {
+  function zero_pad(i, number_of_zeroes) {
+    var ret = i + '';
+    while (ret.length < number_of_zeroes) {
+      ret = '0' + ret;
+    }
+    return ret;
+  }
+
+  if (rec.type == 'Recipe' && rec.published) {
+    var doc = new Document();
+
+    doc.add(rec.summary);
+    doc.add(rec.instructions);
+
+    doc.add(rec.title);
+    doc.add(rec.title, {"field":"title", "store":"yes"});
+    doc.add(rec.title, {"field":"sort_title", "index":"not_analyzed"});
+
+    doc.add(rec.date);
+    doc.add(rec.date, {"field":"date", "store":"yes"});
+    doc.add(rec.date, {"field":"sort_date", "index":"not_analyzed"});
+
+    doc.add(rec.prep_time);
+    doc.add(rec.prep_time, {"store":"yes", "field":"prep_time"});
+    doc.add(zero_pad(rec.prep_time, 5), {"field":"sort_prep", "index":"not_analyzed"});
+
+    // doc.add((rec.tag_names || []).join(' '), {"field":"category"}); */
+
+    var ingredients = [];
+    for (var i=0; i<rec.preparations.length; i++) {
+      ingredients.push(rec.preparations[i]['ingredient']['name']);
+    }
+    doc.add(ingredients.join(', '));
+    doc.add(ingredients.join(', '), {"store":"yes", "field":"ingredient"});
+    var ingredient_count = doc['preparations'] ? doc['preparations'].length : 0;
+    doc.add(zero_pad(ingredient_count, 5), {"field":"sort_ingredient", "index":"not_analyzed"});
+
+    doc.add("Recipe", {"field":"type"});
+
+    return doc;
+  }
+  else {
+    return null;
+  }
+}
